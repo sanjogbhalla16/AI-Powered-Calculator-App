8bde85515ab088dc54c111c981dba547d1b72da7
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-04-30 23:43:02 +0530

    Learning about Canvas HTML tag

--- frontend/src/components/CanvasBoard.tsx
+++ frontend/src/components/CanvasBoard.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+import React, { useRef, useEffect, useState } from "react";
+import axios from "axios";
 
-const CanvasBoard = () => {
+const CanvasBoard: React.FC = () => {
+  //useRef is a React Hook that lets you reference a value that's not needed for rendering.
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const [isDrawing, setIsDrawing] = useState(false);
+
+  //useEffect runs on every render. That means that when the count changes, a render happens, which then triggers another effect.
+  useEffect(() => {}, []);
   return <div>CanvasBoard</div>;
 };
 
