d6c1afd15a7412a2d19ae11db3dfc8d8542da7fe
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-05-01 02:03:21 +0530

    Made the Canvas Board Component

--- frontend/src/components/CanvasBoard.tsx
+++ frontend/src/components/CanvasBoard.tsx
@@ -8,8 +8,52 @@ const CanvasBoard: React.FC = () => {
   const [isDrawing, setIsDrawing] = useState(false);
 
   //useEffect runs on every render. That means that when the count changes, a render happens, which then triggers another effect.
-  useEffect(() => {}, []);
-  return <div>CanvasBoard</div>;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    ctx.fillStyle = "pink";
+    ctx.fillRect(10, 10, 150, 100);
+
+    const handleMouseDown = (e: MouseEvent) => {
+      setIsDrawing(true);
+      ctx.beginPath();
+      ctx.moveTo(e.offsetX, e.offsetY);
+    };
+
+    const handleMouseMove = (e: MouseEvent) => {
+      if (!isDrawing) return;
+      ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+    };
+    const handleMouseUp = () => {
+      setIsDrawing(false);
+    };
+
+    canvas.addEventListener("mousedown", handleMouseDown);
+    canvas.addEventListener("mousemove", handleMouseMove);
+    canvas.addEventListener("mouseup", handleMouseUp);
+
+    return () => {
+      canvas.removeEventListener("mousedown", handleMouseDown);
+      canvas.removeEventListener("mousemove", handleMouseMove);
+      canvas.removeEventListener("mouseup", handleMouseUp);
+    };
+  }, [isDrawing]);
+  return (
+    <div>
+      <h2 className="text-xl font-bold mb-2">Draw on the Canvas</h2>
+      <canvas
+        ref={canvasRef}
+        width={500}
+        height={300}
+        style={{ border: "1px solid black", cursor: "crosshair" }}
+      />
+    </div>
+  );
 };
 
 export default CanvasBoard;
